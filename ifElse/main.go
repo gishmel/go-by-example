4febebddc5709a51d40bc83965aa533e4ad59dad
gishmel <lukeagreenleaf@gmail.com>
2017-04-22 22:14:43 -0400

    Here are the examples I wrote down looking at the awesome website: www.gobyexample.com

---
+++ ifElse/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+// There is no ternary if in Go, so you'll need to use a full if statement even for basic conditions
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
